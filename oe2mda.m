433a5da12250a1a26153b03458e328d512ddd674
Joram van Rheede <joramvanrheede@gmail.com>
2018-05-22 14:23:14 +0100

    reduced functionality to only do openephys to mda conversion - keeping functions clean and simple

--- oe2mda.m
+++ oe2mda.m
@@ -34,27 +34,6 @@ for a = 1:length(channels)
     disp('writing mda file')
     writemda(data',write_file)
     
-    % Run mountainlab processes using a string of system commands; all as
-    % one long command to system because otherwise matlab executes each
-    % statement in isolation rather than letting them build on one another
-    % (i.e. it forgets the path and virtual environment).
-    % Any relevant .bash_profile statements need to be included too.
-    system(['export PATH=/Applications/mountainlab-js/bin/:$PATH ; ' ...    % Duplicate from ~/.bash_profile
-        'export PATH=~/.mountainlab/packages/ephys-viz/bin/:$PATH ; ' ...   % Duplicate from ~/.bash_profile
-        'export PATH=/usr/local/bin/:$PATH ; ' ...                          % Duplicate from ~/.bash_profile
-        'export PATH=~/.mountainlab/packages/:$PATH ; ' ...                 % Duplicate from ~/.bash_profile
-        'export PATH=/usr/local/lib/python3.6/site-packages/:$PATH ; ' ...  % Duplicate from ~/.bash_profile
-        'alias python=''python3'' ; ' ...                                   % Duplicate from ~/.bash_profile
-        'source ~/ml_venv/bin/activate ; ' ...                              % Start previously created ml_venv virtual environment for running mountainlab
-        'cd ' save_loc ' ; ' ...                                            % cd to working directory
-        
-        % Use ephys.bandpass_filter to filter data
-        'ml-run-process ephys.bandpass_filter --inputs timeseries:' write_file ' --outputs timeseries_out:./filt_' chan_nr '.mda.prv --parameters samplerate:30000 freq_min:300 freq_max:6000 ; ' ...
-        % Use ephys.whiten to whiten the data
-        'ml-run-process ephys.whiten --inputs timeseries:./filt_' chan_nr '.mda.prv --outputs timeseries_out:./pre' chan_nr '.mda.prv ;' ...
-        % Use ms4alg.sort for spike detection, clustering and sorting
-        'ml-run-process ms4alg.sort --inputs timeseries:./pre' chan_nr '.mda.prv  --outputs firings_out:./firings' chan_nr '.mda.prv --parameters adjacency_radius:0 detect_sign:-1 detect_threshold:' num2str(detect_threshold)]);
-        
 end
 
 
