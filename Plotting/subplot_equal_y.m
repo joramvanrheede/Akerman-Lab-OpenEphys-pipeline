2eb109034503dbc80ade5ca648e8bff8fe1db793
Joram van Rheede <joramvanrheede@gmail.com>
2019-05-15 17:34:35 +0100

    Prevent error for single plot figures

--- Plotting/subplot_equal_y.m
+++ Plotting/subplot_equal_y.m
@@ -1,11 +1,18 @@
 function subplot_equal_y
-% sets all subplots in figure to use the same y axis
+% function subplot_equal_y
+% Sets all subplots in figure to use the same y axis; does nothing if there
+% is only a single plot in the figure so far.
 
 plotaxes    = get(gcf,'Children');
-maxy        = cellfun(@max,get(plotaxes,'Ylim'));
-miny        = cellfun(@min,get(plotaxes,'Ylim'));
 
-maxy        = max(maxy);
-miny        = min(miny);
+y_limits    = get(plotaxes,'Ylim');
 
-set(plotaxes,'YLim',[miny maxy]); % add margin
+if iscell(y_limits)
+    maxy        = cellfun(@max,y_limits);
+    miny        = cellfun(@min,y_limits);
+    
+    maxy        = max(maxy);
+    miny        = min(miny);
+    
+    set(plotaxes,'YLim',[miny maxy]); % add margin
+end
