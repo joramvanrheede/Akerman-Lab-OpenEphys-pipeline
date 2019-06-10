875db7da9ca64d87a521e225569795ef2e471d65
Joram van Rheede <joramvanrheede@gmail.com>
2019-06-10 13:22:55 +0100

    Finish integration of response onset argument

--- Quality control/burst_control.m
+++ Quality control/burst_control.m
@@ -10,18 +10,28 @@ function q_burst = burst_control(spikes, burst_win, burst_thresh, resp_onset)
 % q_burst: a vector of length n_trials which is TRUE for bursts and FALSE
 % for non-bursts
 
+% Default response onset = 0 (assumes 'spikes' has already been zeroed on response onset)
 if nargin < 4
     resp_onset = 0;
 end
+
+% Default is a single spike disqualifies the response
 if nargin < 3
     burst_thresh = 1;
 end
 
+% set actual response onset to 0
+spikes                      = spikes - resp_onset;
+
+% Create matrix that is 1 for spike times within the window for detecting bursts & 0 otherwise
 spikes_in_burst_win         = spikes > burst_win(1) & spikes <= burst_win(2);
 
+% Sum over n_spikes dimension, should give a nr of spikes within window per channel and per trial
 burst_spikes                = sum(spikes_in_burst_win,3);
 
+% Now sum over channels 
 burst_spikes_per_trial      = sum(burst_spikes,1);
 
+% Use threshold to decide on bursts
 q_burst                     = burst_spikes_per_trial >= burst_thresh;
 
