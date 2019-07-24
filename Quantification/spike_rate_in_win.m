96d1e92fe4c86ff61529300e2cc1973c64ad64ce
Joram van Rheede <joramvanrheede@gmail.com>
2019-07-24 11:59:58 +0100

    add optional standard error output argument

--- Quantification/spike_rate_in_win.m
+++ Quantification/spike_rate_in_win.m
@@ -1,8 +1,9 @@
-function spike_rate = spike_rate_in_win(spikes, time_win)
-% function spike_rate = spike_rate_in_win(SPIKES, TIME_WIN)
+function [spike_rate, spike_rate_serr] = spike_rate_in_win(spikes, time_win)
+% function [spike_rate, spike_rate_serr] = spike_rate_in_win(SPIKES, TIME_WIN)
 % 
 % Returns SPIKE_RATE in Hz in a given TIME_WIN. Spike rate is average 
-% for all channels and all trials in SPIKES
+% for all channels and all trials in SPIKES. Also returns SPIKE_RATE_SERR, 
+% the standard error of the mean over trials.
 % 
 % SPIKES: a N_CHANNELS * N_TRIALS * N_SPIKES matrix of spike times, 
 % padded with NaNs for empty values. Function assumes spike times are in
@@ -12,16 +13,15 @@ function spike_rate = spike_rate_in_win(spikes, time_win)
 % TSPIKE >= T1 to TSPIKE <= T2
 
 % Count spike times within time window
-spikes_in_win       = spikes >= time_win(1) & spikes <= time_win(2);
-spike_count         = sum(spikes_in_win(:));
-
-% Work out mean spike count over trials and channels
-n_channels          = size(spikes,1);
-n_trials            = size(spikes,2);
-mean_spike_count  	= spike_count / n_channels / n_trials;
+spikes_in_win                       = spikes >= time_win(1) & spikes <= time_win(2);
+spike_counts_by_trial_by_channel    = sum(spikes_in_win,3);
+spike_counts_by_trial               = mean(spike_counts_by_trial_by_channel,1)';
+mean_spike_count                    = mean(spike_counts_by_trial);
+serr_spike_count                    = serr(spike_counts_by_trial);
 
 % How long is time window?
 delta_t             = time_win(2) - time_win(1);
 
 % Convert to rate by dividing by delta_t
 spike_rate          = mean_spike_count / delta_t;
+spike_rate_serr     = serr_spike_count / delta_t;
