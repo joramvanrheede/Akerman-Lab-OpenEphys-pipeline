7de917db4aa186abe1b812d713d1ca32c93eba39
Joram van Rheede <joramvanrheede@gmail.com>
2019-07-24 17:11:55 +0100

    bug fix

--- Miscellaneous/serr.m
+++ Miscellaneous/serr.m
@@ -1,16 +1,27 @@
-function stderror = serr(input)
+function stderror = serr(input, dim)
 
-% function stderror = serr(input)
+% function stderror = serr(input,dim)
 % 
 % Quick and dirty standard error function.
 % Ignores NaNs.
-% Works on vectors or on first non-singleton dimension of input.
+% Works on vectors or on first non-singleton dimension of input unless 'dim'
+% is specified.
+% If 'dim' is 'all', will operate on all elements of input.
 
-% find standard deviation of input
-instd       = nanstd(input);
+if nargin <2
+    % find standard deviation of input
+    instd       = nanstd(input);
+    % determine n (non-NaN)
+    n           = sum(~isnan(input));
+else
+    % find standard deviation of input
+    instd       = nanstd(input,[],dim);
+    % determine n (non-NaN)
+    n           = sum(~isnan(input),dim);
+end
 
-% determine n (non-NaN)
-n           = sum(~isnan(input));
+
 
 % divide by square root of n
 stderror    = instd ./ sqrt(n);
+
