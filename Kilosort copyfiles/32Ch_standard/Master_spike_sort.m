55732f1491971d7675cc0d1693da74094f24e37d
Joram van Rheede <joramvanrheede@gmail.com>
2019-10-03 17:23:48 +0100

    Better commenting / help

--- Kilosort copyfiles/32Ch_standard/Master_spike_sort.m
+++ Kilosort copyfiles/32Ch_standard/Master_spike_sort.m
@@ -1,15 +1,21 @@
+% Master_spike_sort
+% 
+% This will run the Kilosort algorithm. Adapted from Kilosort's own script.
+% Must be run from the folder containing the data; run_Kilosort_AkermanLab 
+% will copy this file to the target data directory and run it.
+
 % Run config file
 run('StandardConfig.m')
 
 tic; % start timer
 
 if ops.GPU     
-    gpuDevice(1); % initialize GPU (will erase any existing GPU arrays)
+    gpuDevice(1);   % initialize GPU (will erase any existing GPU arrays)
 end
 
-[rez, DATA, uproj] = preprocessData(ops); % preprocess data and extract spikes for initialization
-rez                = fitTemplates(rez, DATA, uproj);  % fit templates iteratively
-rez                = fullMPMU(rez, DATA);% extract final spike times (overlapping extraction)
+[rez, DATA, uproj] = preprocessData(ops);              	% preprocess data and extract spikes for initialization
+rez                = fitTemplates(rez, DATA, uproj);    % fit templates iteratively
+rez                = fullMPMU(rez, DATA);               % extract final spike times (overlapping extraction)
 
 % save matlab results file
 save(fullfile(ops.root,  'rez.mat'), 'rez', '-v7.3');
