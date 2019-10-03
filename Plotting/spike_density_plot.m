5a1b121dde31c4283096e9502d0b4e685080e2d8
Joram van Rheede <joramvanrheede@gmail.com>
2019-10-03 14:05:50 +0100

    Converted spike counts/bin in SDP to spike rates/bin as it is a more general measure

--- Plotting/spike_density_plot.m
+++ Plotting/spike_density_plot.m
@@ -1,5 +1,5 @@
-function [image_handle, count_data] = spike_density_plot(spike_data,split_dim,bins)
-% FUNCTION [IMAGE_HANDLE, COUNT_DATA] = SPIKE_DENSITY_PLOT(SPIKE_DATA,SPLIT_DIM,BINS)
+function [image_handle, rate_data] = spike_density_plot(spike_data,split_dim,bins, make_plot)
+% FUNCTION [IMAGE_HANDLE, COUNT_DATA] = SPIKE_DENSITY_PLOT(SPIKE_DATA,SPLIT_DIM,BINS,MAKE_PLOT)
 % 
 % Make heatmap of spike density over time by trial or by channel; 
 % effectively this turns a set of PSTHs into a visually pleasing image that
@@ -26,12 +26,15 @@ function [image_handle, count_data] = spike_density_plot(spike_data,split_dim,bi
 % generate the histogram that determines the colour in the colour map.
 % e.g. for BINS == [0:0.001:1] the function outputs a spike density plot
 % of spikes falling between 0 and 1s, with 1ms time bins.
+%
+% MAKE_PLOT: Optional logical input ('true'/'false'). Is 'true' by default.
+% If 'false', the function does not make a plot and only returns the count data
 % 
 % 
 % IMAGE_HANDLE: A handle to the image plotted with MATLAB's IMAGESC function.
 % 
-% COUNT_DATA: The histogram count data that underlies the heatmap in the
-% image. Use imagesc(COUNT_DATA) to recreate the heatmap.
+% RATE_DATA: The spike rate per bin data that underlies the heatmap in the
+% image. Use imagesc(RATE_DATA) to recreate the heatmap.
 % 
 % Joram van Rheede - March 2019
 
@@ -46,12 +49,19 @@ if nargin < 3
     [temp_counts, bins] = histcounts(spike_data(:),100);
 end
 
+if nargin < 4
+    make_plot   = 1;
+end
+
 % determine size of the heatmap
-n_rows  = size(spike_data, split_dim);
-n_bins  = length(bins);
+n_rows      = size(spike_data, split_dim);
+n_bins      = length(bins);
+
+n_summed    = size(spike_data,3 - split_dim); % how many [channels or trials] are summed for the counts?
+bin_diff    = median(diff(bins)); % how big is a bin
 
 % initialise count_data that will make up the heatmap image
-count_data = zeros(n_rows, n_bins-1); 
+rate_data  = zeros(n_rows, n_bins-1); 
 
 % loop over each row
 for a = 1:n_rows
@@ -63,37 +73,40 @@ for a = 1:n_rows
         trial_spikes      	= spike_data(:,a,:);
     end
     
-    % Make histogram for this row
-    count_data(a,:)     = histcounts(trial_spikes,bins);
+    % Make histogram for this row; convert to rate per trial
+    rate_data(a,:)     = histcounts(trial_spikes,bins) / n_summed / bin_diff;
     
 end
 
-% Display the count data as an image
-image_handle    = imagesc(count_data);
-
-% Do plot aesthetics BEFORE changing tick labels (font size change will change the number of ticks and mess up labeling)
-set(gca,'LineWidth',2,'FontSize',16,'FontName','Helvetica','FontWeight','Bold','TickDir','out','box','off')
-
-% X axis tick labels will currently label the pixel nr in the image
-x_tick_labels   = xticklabels;
-
-% Substitute the current X axis tick labels with the relevant bin so that
-% they reflect the input data (spike times) rather than the image pixel nr.
-new_x_tick_labels   = cell(size(x_tick_labels));
-for i = 1:length(x_tick_labels)
-    x_tick_ind              = str2num(x_tick_labels{i});
-    x_tick_val              = bins(x_tick_ind+1);
-    new_x_tick_labels{i}    = num2str(x_tick_val);
+if make_plot
+    % Display the count data as an image
+    image_handle    = imagesc(rate_data);
+    
+    % Do plot aesthetics BEFORE changing tick labels (font size change will change the number of ticks and mess up labeling)
+    set(gca,'LineWidth',2,'FontSize',16,'FontName','Helvetica','FontWeight','Bold','TickDir','out','box','off')
+    
+    % X axis tick labels will currently label the pixel nr in the image
+    x_tick_labels   = xticklabels;
+    
+    % Substitute the current X axis tick labels with the relevant bin so that
+    % they reflect the input data (spike times) rather than the image pixel nr.
+    new_x_tick_labels   = cell(size(x_tick_labels));
+    for i = 1:length(x_tick_labels)
+        x_tick_ind              = str2num(x_tick_labels{i});
+        x_tick_val              = bins(x_tick_ind+1);
+        new_x_tick_labels{i}    = num2str(x_tick_val);
+    end
+    
+    % Replace the tick labels and do some other aesthetic stuff
+    set(gca,'TickDir','out','Box','off','XTickLabel',new_x_tick_labels)
+    
+    % Because your spike data are hot stuff:
+    colormap hot
+    
+    % invert y axis so that figure can be read from top to bottom (i.e. top row
+    % is the first / most superficial channel, or the first trial / sweep)
+    axis ij
+    
+else
+    image_handle = [];
 end
-
-% Replace the tick labels and do some other aesthetic stuff
-set(gca,'TickDir','out','Box','off','XTickLabel',new_x_tick_labels)
-
-% Because your spike data are hot stuff:
-colormap hot
-
-% invert y axis so that figure can be read from top to bottom (i.e. top row
-% is the first / most superficial channel, or the first trial / sweep)
-axis ij 
-
-
