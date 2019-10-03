ce09f989d9bb45fc7f603d82a076aa1e13b7e4ed
Joram van Rheede <joramvanrheede@gmail.com>
2019-10-03 16:07:53 +0100

    Cleared clutter from spike sort running file

--- Kilosort copyfiles/32Ch_standard/Master_spike_sort.m
+++ Kilosort copyfiles/32Ch_standard/Master_spike_sort.m
@@ -1,34 +1,15 @@
-% default options are in parenthesis after the comment
-
-addpath(genpath('/Users/Joram/Dropbox/Akerman Postdoc/Code/Kilosort')) % path to kilosort folder
-addpath(genpath('/Users/Joram/Documents/MATLAB/npy-matlab')) % path to npy-matlab scripts
-
-pathToYourConfigFile = ''; % take from Github folder and put it somewhere else (together with the master_file)
-run(fullfile(pathToYourConfigFile, 'StandardConfig.m'))
+% Run config file
+run('StandardConfig.m')
 
 tic; % start timer
-%
+
 if ops.GPU     
     gpuDevice(1); % initialize GPU (will erase any existing GPU arrays)
 end
 
-% % uncomment if not using pre-converted data
-% if strcmp(ops.datatype , 'openEphys')
-%    ops = convertOpenEphysToRawBInary(ops);  % convert data, only for OpenEphys
-% end
-% 
-% % do common average referencing:
-% applyCARtoDat([ops.fbinary '.dat'],ops.Nchan,cd);
-% ops.fbinary = [ops.fbinary '_CAR'];
-
-%
 [rez, DATA, uproj] = preprocessData(ops); % preprocess data and extract spikes for initialization
 rez                = fitTemplates(rez, DATA, uproj);  % fit templates iteratively
 rez                = fullMPMU(rez, DATA);% extract final spike times (overlapping extraction)
-
-
-%% Uncomment to implement - AutoMerge. rez2Phy will use for clusters the new 5th column of st3 if you run this)
-% rez = merge_posthoc2(rez);
 
 % save matlab results file
 save(fullfile(ops.root,  'rez.mat'), 'rez', '-v7.3');
@@ -38,4 +19,3 @@ rezToPhy(rez, cd);
 
 % remove temporary file
 delete(ops.fproc);
-%%
