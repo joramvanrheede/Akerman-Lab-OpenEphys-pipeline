bb7b0ff7b634bc97b03fbca54fd65a5f92964396
Joram van Rheede <joramvanrheede@gmail.com>
2019-10-29 14:12:23 +0000

    add support for user-defined CLims as input argument

--- Plotting/Plotting_toolbox/subplot_equal_clims.m
+++ Plotting/Plotting_toolbox/subplot_equal_clims.m
@@ -1,7 +1,12 @@
-function subplot_equal_clims
-% function subplot_equal_clims
-% Sets all subplots in figure to use the same color scale; does nothing if 
-% there is only a single axes object in the figure.
+function subplot_equal_clims(colour_range)
+% function SUBPLOT_EQUAL_CLIMS(COLOUR_RANGE)
+% 
+% SUBPLOT_EQUAL_CLIMS by itself sets all subplots in figure to use the same 
+% color scale, equal to the maximum colour range between all plots. Does nothing 
+% if there is only a single axes object in the figure.
+% 
+% If COLOUR_RANGE is specified as [cmin cmax], all subplots will be set to 
+% this specified COLOUR RANGE.
 
 % Get the 'children' of the figure object (any object in the figure)
 plotchildren    = get(gcf,'Children');
@@ -17,15 +22,22 @@ color_lims      = get(plotaxes,'Clim');
 % Conditional - if there is only one axes object in the figure, color_lims
 % is a 2-element vector
 if iscell(color_lims)
-    
-    % Get max and minimum values of the color scales for each axes object
-    max_clim    = cellfun(@max,color_lims);
-    min_clim    = cellfun(@min,color_lims);
-    
-    % Get the overal maximum and minimum between axes objects
-    max_clim  	= max(max_clim);
-    min_clim   	= min(min_clim);
-    
+    if nargin < 1
+        % Get max and minimum values of the color scales for each axes object
+        max_clim    = cellfun(@max,color_lims);
+        min_clim    = cellfun(@min,color_lims);
+        
+        % Get the overal maximum and minimum between axes objects
+        max_clim  	= max(max_clim);
+        min_clim   	= min(min_clim);
+    else
+        min_clim    = colour_range(1);
+        max_clim    = colour_range(2);
+    end
     % Set all axes to overall maximum and minimum color range
     set(plotaxes,'CLim',[min_clim max_clim]);
+else
+    if nargin == 1
+        set(plotaxes,'CLim',colour_range);
+    end
 end
