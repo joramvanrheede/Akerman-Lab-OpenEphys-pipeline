cb2bcfe9bb4c4cd3871c7ad4a32190c33c10ce4f
Alexvonklemperer <48834771+Alexvonklemperer@users.noreply.github.com>
2020-04-07 17:44:30 +0200

    Update Current_Source_Density.m

--- AVK_LFP/Current_Source_Density.m
+++ AVK_LFP/Current_Source_Density.m
@@ -1,4 +1,4 @@
-function[CSD,w,sinks,sources,analysis_win] = Current_Source_Density(Traces,CSD_window);
+function[CSD,w,sinks,sources,LFP_sinks] = Current_Source_Density(Traces,CSD_window);
 %% CSD,w = Current_Source_Density(Traces,CSD_window)
 %  extracts current source density for LFP
 % 
@@ -34,6 +34,7 @@ function[CSD,w,sinks,sources,analysis_win] = Current_Source_Density(Traces,CSD_w
 delta_z = 0.025; %um
 sigma = -0.3; %mS/mm
 
+
 for k = 1 : size(Traces,2)
     temp_traces = squeeze(Traces(:,k,:));
         t = 1:size(temp_traces,2); 
@@ -41,13 +42,14 @@ for k = 1 : size(Traces,2)
     CSD_temp(:,:) = sigma*(temp_traces(z-1,t) - 2*temp_traces(z,t) + temp_traces(z+1,t))/(delta_z^2);
     CSD(:,:,k) = CSD_temp;
 end;
-    CSD = nanmean(CSD,3);
-    CSD_window = CSD_window+Artifact_delay;
-    w = squeeze(nanmean(Traces(:,:,:),2));
+    
+CSD = nanmean(CSD,3);
+CSD_window = CSD_window+Artifact_delay;
+w = squeeze(nanmean(Traces(:,:,:),2));
 
   for k = 1 : size(CSD,1) 
     sinks(k) = min(CSD(k,CSD_window(1):CSD_window(2)));
     sources(k) = max(CSD(k,CSD_window(1):CSD_window(2)));
-end;
-analysis_win = CSD_window;
+    LFP_sinks(k) = min(w(k,CSD_window(1):CSD_window(2)));
+   end;
 end    
