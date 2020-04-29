a5604a36e99800e6b6bfa02ea28a3e24d85cb639
Alex von klemperer <48834771+Alexvonklemperer@users.noreply.github.com>
2020-04-29 17:04:45 +0200

    Update Unit_Analysis_new.m

--- Analysis_scripts/Unit_Analysis_new.m
+++ Analysis_scripts/Unit_Analysis_new.m
@@ -8,6 +8,7 @@ if reload == 1
     clear all
 load_folder = 'F:\Synced\Synced_individual'
 group_folders     	= {'POM';'Cortical'}; 
+target_opto = '60'
 reload =1;
 end;
 
@@ -28,7 +29,7 @@ L5_chans            = 5:12; % Layer 5 channels relative to the L4 sink (sink cha
 %% Script starts : 
 if reload == 1;
     for k = 1: numel(group_folders)
-    load_fn = [load_folder '\' group_folders{k} '_selected_exps.mat']
+    load_fn = [load_folder '\' group_folders{k} '_selected_exps_' target_opto '.mat']
     disp(['Loading...' load_fn]);
     load(load_fn);
     groups(k) = selected_exps;
@@ -71,97 +72,5 @@ for a = 1: numel(groups);
 end;
 
 
-%{
-figure('Name','Spike_rate');
-
-clear sig_delta;
-    sig_delta(1:2,1:128,1:7) = NaN;
-    sig_combined(1:2,1:128) = NaN;
-     sig_delta_supergran(1:2,1:128,1:7) = NaN;
-for a = 1:2;
-    unit_count =numel(Analysed_group(a).all_units);
-    cond_count = numel(Analysed_group(a).all_units(1).conditions);
- for b = 1: unit_count;
-     for k = 1 : cond_count;
-     subplot(2,1,a);
-     title(group_folders{a});
-         hold on;
-         sig_index = Analysed_group(a).all_units(b).conditions(k).trial_rate_h;
-         unit_depth = Analysed_group(a).all_units(b).unit_depth;
-     hold on;
-     refline(0,1);
-     plot(k,Analysed_group(a).all_units(b).conditions(k).delta_whisk_rate,'bo');
-     if sig_index == true;
-     sig_delta(a,b,k) = Analysed_group(a).all_units(b).conditions(k).delta_whisk_rate;
-       if unit_depth >400
-           plot(k,Analysed_group(a).all_units(b).conditions(k).delta_whisk_rate,'r*');
-       end;
-         if unit_depth <400
-            plot(k,Analysed_group(a).all_units(b).conditions(k).delta_whisk_rate,'g*');
-            sig_delta_supergran(a,b,k) = Analysed_group(a).all_units(b).conditions(k).delta_whisk_rate;
-         end;
-     end;
-     end;
-     subplot(2,1,a);
-     hold on;
-     if Analysed_group(a).all_units(b).combined_rate_h == 1 & Analysed_group(a).all_units(b).unit_depth <400;
-         plot(4.5,nanmean(Analysed_group(a).all_units(b).combined_response),'g*');
-         sig_combined(a,b) = nanmean(Analysed_group(a).all_units(b).combined_response);
-     end;
-     
- end;
-end;
-
-figure('Name','Spike prob');
- sig_delta_prob(1:2,1:128,1:7) = NaN;
-   
-for a = 1:2;
-    unit_count =numel(Analysed_group(a).all_units);
-    cond_count = numel(Analysed_group(a).all_units(1).conditions);
- for b = 1: unit_count;
-     for k = 1 : cond_count;
-     subplot(2,1,a);
-     title([group_folders{a} 'Probability']);
-         hold on;
-         sig_index = Analysed_group(a).all_units(b).conditions(k).prob_p;
-         unit_depth = Analysed_group(a).all_units(b).unit_depth;
-     hold on;
-     refline(0,1);
-     plot(k,Analysed_group(a).all_units(b).conditions(k).prob_delta,'bo');
-     if sig_index == true;
-     sig_delta_prob(a,b,k) = Analysed_group(a).all_units(b).conditions(k).prob_delta;
-       if unit_depth >400
-           plot(k,Analysed_group(a).all_units(b).conditions(k).prob_delta,'r*');
-       end;
-         if unit_depth <400
-            plot(k,Analysed_group(a).all_units(b).conditions(k).prob_delta,'g*');
-         end;
-     end;
-     end; 
- end;
-end;
-
-figure('Name','Spike latency');
-   
-for a = 1:2;
-    unit_count =numel(Analysed_group(a).all_units);
-    cond_count = numel(Analysed_group(a).all_units(1).conditions);
- for b = 1: unit_count;
-     for k = 1 : cond_count;
-     subplot(2,1,a);
-     title([group_folders{a} 'Spike_latency']);
-         hold on;
-   refline(0,0);
-         plot(k,Analysed_group(a).all_units(b).conditions(k).Spike_1_delta,'bo');
-     end; 
- end;
-end;
 
 
-for k = 1 : 6
-stat_h(k) = ranksum(sig_delta_supergran(1,:,k),sig_delta(2,:,k));
-stat_prob_h(k) = ranksum(sig_delta_prob(1,:,k),sig_delta_prob(2,:,k));
-end;
-
-[h,p] = ranksum(sig_combined(1,:),sig_combined(2,:));
-%}
